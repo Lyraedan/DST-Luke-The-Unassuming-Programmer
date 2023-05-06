387880d07fe32c0be042eda5bac7ab9b2607aa6d
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2023-05-06 19:44:45 +0100

    Updated forum thread and added more art assets

--- Code/modinfo.lua
+++ Code/modinfo.lua
@@ -5,7 +5,7 @@ author = "Matilde, ItsLuke"
 version = "1.0.10" -- This is the version of the template. Change it to your own number.
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
-forumthread = ""
+forumthread = "https://github.com/Lyraedan/DST-Luke-The-Unassuming-Programmer"
 
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
 api_version = 10
